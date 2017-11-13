3ab5aba5e7bf3b069743ce1989e6807c01d6c8f2
norman <vincenzonorman@gmail.com>
2017-11-13 12:19:49 +0100

    Add ignore warnings for matlab script, and matlab scripts directory renamed

--- matlab_scripts/getStopDistanceSGR.m
+++ matlab_scripts/getStopDistanceSGR.m
@@ -1,4 +1,5 @@
 function [ stop_distance ] = getStopDistanceSGR( inputs )
+    warning('off', 'all');
     load('dataXRun.mat');
     stop_distance = getStopDistanceXRun(inputs, bottomModel,anfisModel);
 end
